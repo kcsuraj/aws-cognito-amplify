d96333976fc73750f4708f1feaa437f8f8aacee4
Suraj kc <isurajkc@gmail.com>
2020-05-28 12:00:46 +0545

    Updates

---
+++ precache-manifest.e1d1d148fcd64d58d552313376c2c15b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660c21e566d3300cd9ea1ad06587dd69",
+    "url": "/aws-cognito-amplify/index.html"
+  },
+  {
+    "revision": "70cd2a3ed9d4fabbe926",
+    "url": "/aws-cognito-amplify/static/css/main.2ae118ae.chunk.css"
+  },
+  {
+    "revision": "814f069fcb99ad179803",
+    "url": "/aws-cognito-amplify/static/js/10.4ea1fbd4.chunk.js"
+  },
+  {
+    "revision": "d22022f0492209e5546f",
+    "url": "/aws-cognito-amplify/static/js/11.b0182b5d.chunk.js"
+  },
+  {
+    "revision": "b8c558add1c9cb882d0c",
+    "url": "/aws-cognito-amplify/static/js/12.ef88aa18.chunk.js"
+  },
+  {
+    "revision": "c6cf6378e47515508ddd",
+    "url": "/aws-cognito-amplify/static/js/13.863d33f3.chunk.js"
+  },
+  {
+    "revision": "b34c49f9012264f25709",
+    "url": "/aws-cognito-amplify/static/js/14.f1bb6f14.chunk.js"
+  },
+  {
+    "revision": "d453856bfe5ac001a076",
+    "url": "/aws-cognito-amplify/static/js/15.d3ce4e1d.chunk.js"
+  },
+  {
+    "revision": "2c40793da1ec647eb3e7",
+    "url": "/aws-cognito-amplify/static/js/16.f8674cd8.chunk.js"
+  },
+  {
+    "revision": "d3db9590c579976a7b87",
+    "url": "/aws-cognito-amplify/static/js/17.b094c043.chunk.js"
+  },
+  {
+    "revision": "2b23173b49c20a09bad3",
+    "url": "/aws-cognito-amplify/static/js/18.a7a8dd05.chunk.js"
+  },
+  {
+    "revision": "e47b9c717f3464dd7044",
+    "url": "/aws-cognito-amplify/static/js/19.75365389.chunk.js"
+  },
+  {
+    "revision": "0d5deeda8f3552e8b366",
+    "url": "/aws-cognito-amplify/static/js/20.dfe9d472.chunk.js"
+  },
+  {
+    "revision": "f2e14c386585db0b3dfc",
+    "url": "/aws-cognito-amplify/static/js/21.f32a8f4d.chunk.js"
+  },
+  {
+    "revision": "db807a8c1d3a03a634c3",
+    "url": "/aws-cognito-amplify/static/js/22.cc9a7934.chunk.js"
+  },
+  {
+    "revision": "af08e29d4cdfe747cf2e",
+    "url": "/aws-cognito-amplify/static/js/23.29f1fd66.chunk.js"
+  },
+  {
+    "revision": "ba318c67597308c4f9c7",
+    "url": "/aws-cognito-amplify/static/js/24.d71642ef.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/aws-cognito-amplify/static/js/24.d71642ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1ea48841e3647c6430a",
+    "url": "/aws-cognito-amplify/static/js/25.b2efa53d.chunk.js"
+  },
+  {
+    "revision": "d27fdbd4001fa8c86ba9",
+    "url": "/aws-cognito-amplify/static/js/3.1cb0061b.chunk.js"
+  },
+  {
+    "revision": "055fc96228ebd7596dd144de5fa0736f",
+    "url": "/aws-cognito-amplify/static/js/3.1cb0061b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c871423e31b3161fd76",
+    "url": "/aws-cognito-amplify/static/js/4.d7f14f97.chunk.js"
+  },
+  {
+    "revision": "b8941f6fedac6a746de0",
+    "url": "/aws-cognito-amplify/static/js/5.13700ad0.chunk.js"
+  },
+  {
+    "revision": "355c12f7767e0ec2b1cb",
+    "url": "/aws-cognito-amplify/static/js/6.ea46e110.chunk.js"
+  },
+  {
+    "revision": "31f7d68fe99fd2564c7223f77eb19ad7",
+    "url": "/aws-cognito-amplify/static/js/6.ea46e110.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9139b3d4e3a82d129a31",
+    "url": "/aws-cognito-amplify/static/js/7.a3fbc9c7.chunk.js"
+  },
+  {
+    "revision": "dcf6ff2e9e60862f4d9e",
+    "url": "/aws-cognito-amplify/static/js/8.7d99a090.chunk.js"
+  },
+  {
+    "revision": "171402485ff7e490247b",
+    "url": "/aws-cognito-amplify/static/js/9.61f8b49b.chunk.js"
+  },
+  {
+    "revision": "70cd2a3ed9d4fabbe926",
+    "url": "/aws-cognito-amplify/static/js/main.0e01511f.chunk.js"
+  },
+  {
+    "revision": "f3851cc4cf8b62ca128d",
+    "url": "/aws-cognito-amplify/static/js/polyfills-css-shim.e149b3ee.chunk.js"
+  },
+  {
+    "revision": "42b3c3874eccb12fb8c8",
+    "url": "/aws-cognito-amplify/static/js/runtime-main.fb52ba90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aws-cognito-amplify/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bab8885a47c3a1a27a5643f91606c181.js"
+  "/aws-cognito-amplify/precache-manifest.e1d1d148fcd64d58d552313376c2c15b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aws-cognito-amplify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
